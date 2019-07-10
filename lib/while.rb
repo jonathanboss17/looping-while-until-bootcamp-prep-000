821a62dac4ee8c704faf3e2bcdfb0e93329a0abd
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-10 21:46:00 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -4,8 +4,8 @@ def using_while
   #your code here
   
   while levitation_force < 10
-    levitation_force += 1
-    puts "Wingardium Leviosa"
+    puts levitation_force += 1
+    #puts "Wingardium Leviosa"
   end
   
 end
